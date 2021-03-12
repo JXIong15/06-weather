c51b21d27a774158c45b45606523cd6222649b3e
Pikachu <jouxiong15@gmail.com>
2021-03-12 13:38:37 -0600

    added API Key

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+    var API_KEY = "48d3bee5ac7cdd8e92b4de30d8718eca";
+
+
     $("#searchBtn").on("click", function() {
-        alert("Hey!");
+        // let city = $("#city").val();
+        // searchCityWeather(city);
     })
 
+    function searchCityWeather(city) {
+        $.ajax({
+            type: "GET",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`,
+            datatype: "json",
+            success: function(data) {
+                console.log(data);
 
-
+            }
+        })
+    }
+searchCityWeather("chicago");
 })
